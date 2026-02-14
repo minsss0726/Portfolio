3ca7c088179ee4df62bae1df413eaa4cf3c34dea
minsss0726 <wndtmgld5551@gmail.com>
2026-02-15 00:20:23 +0900

    fix: 졸업 정보 변경

--- app/data/education.ts
+++ app/data/education.ts
@@ -12,6 +12,6 @@ export interface Education {
 export const education: Education = {
   school: "인덕대학교(3년제)",
   major: "컴퓨터전자공학과",
-  graduationYear: "24년 졸업",
+  graduationYear: "24년 2월 졸업",
   gpa: "4.0/4.5",
 };
